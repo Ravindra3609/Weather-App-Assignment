ac0aa104c82eb3971d4824cc94e2623626ee6a37
Ravindra <ravindranaidukuriti@gmail.com>
2024-06-06 13:39:53 +0530

    Weather App

---
+++ src/Components/Animation.js
@@ -0,0 +1,11 @@
+import {gsap} from 'gsap';
+
+//set TimeOut()
+
+const Animation = () => {
+    return(
+        <div className='logo'>Weather App - Hiring hood </div>
+    )
+}
+
+export default Animation;
